3f961d14edf5b6e7a8c2b924d65e2bc294afcf04
Sanket <sanket.chouhan555@gmail.com>
2020-10-05 00:29:41 +0530

    web and native app

---
+++ Sharepic web app/build/precache-manifest.5d683de74d397943bd86671109ccaaad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1594a7c22f9d5f1f0daded92d4d60333",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e330b9dfa5c9bd2e84cf",
+    "url": "/static/css/3.34dcc6c8.chunk.css"
+  },
+  {
+    "revision": "5d9ad7030c51e58098ce",
+    "url": "/static/css/4.642b2cae.chunk.css"
+  },
+  {
+    "revision": "49ad87d9667eaa5cb769",
+    "url": "/static/css/5.deb6ae82.chunk.css"
+  },
+  {
+    "revision": "6e0a32ec68158b2e830d",
+    "url": "/static/css/main.fe2223fd.chunk.css"
+  },
+  {
+    "revision": "abb40cd90d4c26d3f3f9",
+    "url": "/static/js/2.cc41f06a.chunk.js"
+  },
+  {
+    "revision": "c6bc5621a97bbab23d23e5b5e5edab23",
+    "url": "/static/js/2.cc41f06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e330b9dfa5c9bd2e84cf",
+    "url": "/static/js/3.7c7f644c.chunk.js"
+  },
+  {
+    "revision": "95c860f7c5114b781b46abadb3ee6e02",
+    "url": "/static/js/3.7c7f644c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9ad7030c51e58098ce",
+    "url": "/static/js/4.422de64f.chunk.js"
+  },
+  {
+    "revision": "49ad87d9667eaa5cb769",
+    "url": "/static/js/5.d04a53ff.chunk.js"
+  },
+  {
+    "revision": "6e0a32ec68158b2e830d",
+    "url": "/static/js/main.2bea5dcb.chunk.js"
+  },
+  {
+    "revision": "ef302a99d66beeaae903",
+    "url": "/static/js/runtime-main.028aa3ab.js"
+  }
+]);
